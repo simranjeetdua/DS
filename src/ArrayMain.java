11352bbca8e0fc1b137d9f5014d72c088cb921a9
Simranjeet Dua <simranjeet.dua.12@gmail.com>
2018-05-14 15:56:03 +0530

    implemented array in java

---
+++ src/ArrayMain.java
@@ -0,0 +1,13 @@
+
+public class ArrayMain {
+
+	public static void main(String[] args) {
+		//to implement arrays in java.
+		
+		int[] arr;
+		arr = new int[5];
+		
+		System.out.println(arr[0]);
+	}
+
+}
