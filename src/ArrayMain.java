bb3c4b131696ba9e9054545bcb42bfdd4ada9950
Simranjeet Dua <simranjeet.dua.12@gmail.com>
2018-05-14 16:12:55 +0530

    .

--- src/ArrayMain.java
+++ src/ArrayMain.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class ArrayMain {
 
 	public static void main(String[] args) {
@@ -7,7 +7,23 @@ public class ArrayMain {
 		int[] arr;
 		arr = new int[5];
 		
-		System.out.println(arr[0]);
-	}
-
+		System.out.println(arr[0]); //thus, java stores the default value as output is 0
+		
+		//or can also be implemented as 
+		
+		int[] arr1=new int[5];
+		
+		//ARRAY LITERAL: WHEN THE SIZE OF ARRAY IS ALREADY KNOWN ARRAY LITERAL CAN BE USED AS DESCRIBED UNDER
+		
+		int[] arrlit= new int[] {1,2,3,4,5,6,7,8};
+		
+		// Java program to illustrate creating an array
+		// of integers,  puts some values in the array,
+		// and prints each value to standard output.
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the size of array");
+		int n=s.nextInt();
+		int[] exarray= new int[n];
+		for(int i=0;i<length.exarray)
+}
 }
